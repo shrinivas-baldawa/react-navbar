35a0130b76de4b71102887ffc9b2cc6c299e985a
Shrinivas <baldawashrinivas19@siesgst.ac.in>
2021-09-15 12:16:22 +0530

    15th Sept 2021, 12:16 PM

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import home from './component/home';
 import about from './component/about';
 import {Route,Switch} from 'react-router-dom';
+import error from './component/error';
 
 function App() {
   return (
     <div className="App">
       <main>
         <Switch>
-          <Route path="/home" component={home}></Route>
+          <Route path="/" component={home} exact></Route>
           <Route path="/about" component={about}></Route>
+          <Route component={error}></Route>
         </Switch>
       </main>
     </div>
---
+++ src/component/error.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+function error(){
+    return(
+        <h1>No such page exists</h1>
+    )
+}
+
+export default error;
