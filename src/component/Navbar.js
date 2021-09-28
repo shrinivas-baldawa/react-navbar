9cf6dfc6a0e9ba8a5a1145ec9c904ff725d9441d
Shrinivas <baldawashrinivas19@siesgst.ac.in>
2021-09-28 11:41:22 +0530

    28th Sept, 2021 11:40 AM

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,9 +5,9 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-1">
             <div className="container-fluid">
+                <h1 className = "navbar-brand">Shobhana Iyer</h1>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <h5 className = "nav-link">Shrinivas</h5>
                             <NavLink className="nav-link" to="/">Home</NavLink>
                             <NavLink className="nav-link" to="/about">About Us</NavLink>
                         </div>
@@ -16,3 +16,10 @@ export default function Navbar() {
         </nav>
     )
 }
+
+
+
+
+
+
+
